3c1706a4a84fb25fbf5627c32e6d2d0a1b124f45
vinicius.trovao <vinicius.trovao@NTMKBR31>
2016-06-27 16:58:35 -0300

    Add project files.

---
+++ CaixaEletronico/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaixaEletronico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Conta umaConta = new Conta();
+            umaConta.numero = 1;
+            umaConta.titular = "Vinicius Trovão";
+            umaConta.saldo = 2000.0;
+
+            Conta outraConta = new Conta();
+            outraConta.numero = 2;
+            outraConta.titular = "Ranay Nobrega";
+            outraConta.saldo = 1800.0;
+
+            umaConta.Transfere(200, outraConta);
+
+
+            MessageBox.Show("O saldo de Vinicius é " + umaConta.saldo);
+            MessageBox.Show("O Saldo de Ranay é " + outraConta.saldo);       
+        }
+    }
+}
