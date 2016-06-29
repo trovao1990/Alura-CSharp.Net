83f3b0802bdb99286800165f0f53b17a8508e3fc
Vinicius Trovao Pereira <vini-trovao2@hotmail.com>
2016-06-29 19:24:17 -0300

    transferencia por combobox

--- CaixaEletronico/Form1.Designer.cs
+++ CaixaEletronico/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.Button btnTransferencia;
             this.textoTitular = new System.Windows.Forms.TextBox();
             this.textoSaldo = new System.Windows.Forms.TextBox();
             this.textoNumero = new System.Windows.Forms.TextBox();
@@ -39,36 +40,37 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.btnTotalizador = new System.Windows.Forms.Button();
-            this.comboContas = new System.Windows.Forms.ComboBox();
+            this.comboOrigem = new System.Windows.Forms.ComboBox();
+            this.comboDestino = new System.Windows.Forms.ComboBox();
+            btnTransferencia = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textoTitular
             // 
-            this.textoTitular.Location = new System.Drawing.Point(133, 43);
+            this.textoTitular.Location = new System.Drawing.Point(161, 68);
             this.textoTitular.Name = "textoTitular";
-            this.textoTitular.Size = new System.Drawing.Size(100, 20);
+            this.textoTitular.Size = new System.Drawing.Size(104, 20);
             this.textoTitular.TabIndex = 1;
             // 
             // textoSaldo
             // 
-            this.textoSaldo.Location = new System.Drawing.Point(133, 95);
+            this.textoSaldo.Location = new System.Drawing.Point(161, 120);
             this.textoSaldo.Name = "textoSaldo";
-            this.textoSaldo.Size = new System.Drawing.Size(100, 20);
+            this.textoSaldo.Size = new System.Drawing.Size(104, 20);
             this.textoSaldo.TabIndex = 2;
             // 
             // textoNumero
             // 
-            this.textoNumero.Location = new System.Drawing.Point(133, 69);
+            this.textoNumero.Location = new System.Drawing.Point(161, 94);
             this.textoNumero.Name = "textoNumero";
-            this.textoNumero.Size = new System.Drawing.Size(100, 20);
+            this.textoNumero.Size = new System.Drawing.Size(104, 20);
             this.textoNumero.TabIndex = 3;
             // 
             // textoValor
             // 
-            this.textoValor.Location = new System.Drawing.Point(133, 121);
+            this.textoValor.Location = new System.Drawing.Point(161, 146);
             this.textoValor.Name = "textoValor";
-            this.textoValor.Size = new System.Drawing.Size(100, 20);
+            this.textoValor.Size = new System.Drawing.Size(104, 20);
             this.textoValor.TabIndex = 4;
             // 
             // btnDeposito
@@ -83,7 +85,7 @@
             // 
             // btnSaque
             // 
-            this.btnSaque.Location = new System.Drawing.Point(173, 184);
+            this.btnSaque.Location = new System.Drawing.Point(147, 183);
             this.btnSaque.Name = "btnSaque";
             this.btnSaque.Size = new System.Drawing.Size(99, 23);
             this.btnSaque.TabIndex = 6;
@@ -94,7 +96,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(36, 50);
+            this.label1.Location = new System.Drawing.Point(64, 75);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(39, 13);
             this.label1.TabIndex = 7;
@@ -103,7 +105,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(38, 98);
+            this.label2.Location = new System.Drawing.Point(66, 123);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(37, 13);
             this.label2.TabIndex = 8;
@@ -112,7 +114,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(34, 72);
+            this.label3.Location = new System.Drawing.Point(62, 97);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(93, 13);
             this.label3.TabIndex = 9;
@@ -121,7 +123,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(38, 124);
+            this.label4.Location = new System.Drawing.Point(66, 149);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(82, 13);
             this.label4.TabIndex = 10;
@@ -137,32 +139,42 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // btnTotalizador
+            // comboOrigem
             // 
-            this.btnTotalizador.Location = new System.Drawing.Point(173, 212);
-            this.btnTotalizador.Name = "btnTotalizador";
-            this.btnTotalizador.Size = new System.Drawing.Size(99, 23);
-            this.btnTotalizador.TabIndex = 12;
-            this.btnTotalizador.Text = "Totalizador";
-            this.btnTotalizador.UseVisualStyleBackColor = true;
-            this.btnTotalizador.Click += new System.EventHandler(this.btnTotalizador_Click);
+            this.comboOrigem.FormattingEnabled = true;
+            this.comboOrigem.Location = new System.Drawing.Point(161, 12);
+            this.comboOrigem.Name = "comboOrigem";
+            this.comboOrigem.Size = new System.Drawing.Size(125, 21);
+            this.comboOrigem.TabIndex = 13;
+            this.comboOrigem.SelectedIndexChanged += new System.EventHandler(this.comboContas_SelectedIndexChanged);
             // 
-            // comboContas
+            // comboDestino
             // 
-            this.comboContas.FormattingEnabled = true;
-            this.comboContas.Location = new System.Drawing.Point(133, 12);
-            this.comboContas.Name = "comboContas";
-            this.comboContas.Size = new System.Drawing.Size(121, 21);
-            this.comboContas.TabIndex = 13;
-            this.comboContas.SelectedIndexChanged += new System.EventHandler(this.comboContas_SelectedIndexChanged);
+            this.comboDestino.FormattingEnabled = true;
+            this.comboDestino.Location = new System.Drawing.Point(161, 40);
+            this.comboDestino.Name = "comboDestino";
+            this.comboDestino.Size = new System.Drawing.Size(125, 21);
+            this.comboDestino.TabIndex = 14;
+            this.comboDestino.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnTransferencia
+            // 
+            btnTransferencia.Location = new System.Drawing.Point(262, 183);
+            btnTransferencia.Name = "btnTransferencia";
+            btnTransferencia.Size = new System.Drawing.Size(85, 53);
+            btnTransferencia.TabIndex = 15;
+            btnTransferencia.Text = "Transferência";
+            btnTransferencia.UseVisualStyleBackColor = true;
+            btnTransferencia.Click += new System.EventHandler(this.btnTransferencia_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.comboContas);
-            this.Controls.Add(this.btnTotalizador);
+            this.ClientSize = new System.Drawing.Size(349, 261);
+            this.Controls.Add(btnTransferencia);
+            this.Controls.Add(this.comboDestino);
+            this.Controls.Add(this.comboOrigem);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -194,8 +206,8 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button btnTotalizador;
-        private System.Windows.Forms.ComboBox comboContas;
+        private System.Windows.Forms.ComboBox comboOrigem;
+        private System.Windows.Forms.ComboBox comboDestino;
     }
 }
 
--- CaixaEletronico/Form1.cs
+++ CaixaEletronico/Form1.cs
@@ -6,32 +6,29 @@ namespace CaixaEletronico
     public partial class Form1 : Form
     {
         Conta[] contas;
-     //  private Conta conta;
+        Conta contaSelecionada;
+        Conta contaDestino;
+
+
         public Form1()
         {
             InitializeComponent();
             
-    }
+         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
-            /*this.conta = new Conta();
-            Cliente cliente = new Cliente("Victor");
-            this.conta.Titular = cliente;
-            this.conta.Deposita(250.0);
-            this.conta.Numero = 1;
-            this.MostraConta(); */ 
 
             contas = new Conta[2];
             contas[0] = new Conta();
             contas[1] = new Conta();
             Cliente c1 = new Cliente("Vinicius");
             Cliente c2 = new Cliente("Ranay");
-            contas[0].Deposita(3000.0);
+           
             contas[0].Numero = 1;
             contas[0].Titular= c1;
-            contas[1] = new Conta();
+            contas[0].Deposita(3000.0);
+
             contas[1].Numero = 2;
             contas[1].Titular = c2;
             contas[1].Deposita(3020.0);
@@ -39,7 +36,11 @@ namespace CaixaEletronico
 
             foreach (Conta c in contas)
             {
-                comboContas.Items.Add(c.Titular.Nome);
+                comboOrigem.Items.Add(c.Numero);
+            }
+            foreach (Conta c in contas)
+            {
+                comboDestino.Items.Add(c.Numero);
             }
 
         }
@@ -51,33 +52,40 @@ namespace CaixaEletronico
         private void btnDeposito_Click(object sender, EventArgs e)
         {
            
-          /*  string textoValorDeposito = textoValor.Text;
+            string textoValorDeposito = textoValor.Text;
             double valorDeposito = Convert.ToDouble(textoValorDeposito);
-            this.conta.Deposita(valorDeposito);
-            this.MostraConta(); */
+            contaSelecionada = this.BuscaContaSelecionada();
+            contaSelecionada.Deposita(valorDeposito);
+            this.MostraConta();
         }
 
         private void btnSaque_Click(object sender, EventArgs e)
-        {    /*
+        {    
             string textoValorSaque = textoValor.Text;
             double valorSaque = Convert.ToDouble(textoValorSaque);
-            this.conta.Saca(valorSaque);
-            this.MostraConta();  */
+            contaSelecionada = this.BuscaContaSelecionada();
+            contaSelecionada.Saca(valorSaque);
+            this.MostraConta();
+
+        }
+
+        private Conta BuscaContaSelecionada()
+        {
+            int indiceSelecionado = comboOrigem.SelectedIndex;
+            return this.contas[indiceSelecionado];
         }
 
         private void MostraConta()
-        { 
-           /* textoTitular.Text = this.conta.Titular.Nome;
-            textoSaldo.Text = Convert.ToString(this.conta.Saldo);
-            textoNumero.Text = Convert.ToString(this.conta.Numero); */
+        {
+            
+            textoTitular.Text = contaSelecionada.Titular.Nome;
+            textoNumero.Text = Convert.ToString(contaSelecionada.Numero);
+            textoSaldo.Text = Convert.ToString(contaSelecionada.Saldo);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-     /*       Conta[] contas = new Conta[2];
-            contas[0] = new Conta();
-            contas[1] = new ContaPoupanca();
-
+    
             contas[0].Deposita(10);
             contas[1].Deposita(100);
 
@@ -112,14 +120,28 @@ namespace CaixaEletronico
 
         private void comboContas_SelectedIndexChanged(object sender, EventArgs e)
         {
-            int indiceSelecionado = comboContas.SelectedIndex;
-            Conta contaSelecionada = contas[indiceSelecionado];
-
+            contaSelecionada = this.BuscaContaSelecionada();
             textoTitular.Text = contaSelecionada.Titular.Nome;
             textoNumero.Text = Convert.ToString(contaSelecionada.Numero);
             textoSaldo.Text = Convert.ToString(contaSelecionada.Saldo);
 
 
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTransferencia_Click(object sender, EventArgs e)
+        {
+            contaSelecionada = this.BuscaContaSelecionada();
+            int indiceContaDestino = comboDestino.SelectedIndex;
+            Conta contaDestino = this.contas[indiceContaDestino];
+            string textoTransferencia = textoValor.Text;
+            double valorTransferencia = Convert.ToDouble(textoTransferencia);
+            contaSelecionada.Transfere(valorTransferencia, contaDestino);
+            this.MostraConta();
         }
     }
 }
